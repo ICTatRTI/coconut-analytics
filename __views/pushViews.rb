f11444f251e18d5502c728bf1670f681e305eba8
mikeymckay <mikeymckay@gmail.com>
2018-02-06 10:30:29 +0300

    Adding views, per db views, and removing polluting login logging

--- __views/pushViews.rb
+++ __views/pushViews.rb
@@ -3,15 +3,13 @@ require 'rubygems'
 require 'couchrest'
 require 'json'
 
-couchdb_server = ARGV[0] || "http://localhost:5984"
-database = ARGV[1] || "coconut-surveillance-zanzibar"
-
-@db = CouchRest.new(couchdb_server).database!(database)
+#@db = CouchRest.new(ARGV[0]).database!(ARGV[1])
 
 #Get all .coffee files
 Dir.glob("*.coffee").each do |view|
   next unless view.match(/__reduce/).nil?
   next if view == "executeViews.coffee"
+  puts view
 
   view_name = view.sub(/\.coffee/,"")
   document_id = "_design/#{view_name}"
@@ -29,21 +27,27 @@ Dir.glob("*.coffee").each do |view|
 
   reduce_file = view.sub(/\.coffee/,"__reduce.coffee")
   if File.exist? reduce_file
-    puts "Adding in reduce: #{reduce_file}"
     reduce = File.read(view.sub(/\.coffee/,"__reduce.coffee"))
     local_view_doc[:views][view_name][:reduce] = reduce
   end
 
+  puts local_view_doc
+
+  # Look for something like this to specify the database
+  # db:wusc-people
+  match = map.match(/^.*db:(.*)/)
+  database_name = $1 if match
+  db = CouchRest.new(ARGV[0]).database!(database_name)
   begin
-    db_view_doc = @db.get(document_id)
+    db_view_doc = db.get(document_id)
     local_view_doc["_rev"] = db_view_doc["_rev"] if db_view_doc
   rescue
   end
     
   #puts local_view_doc.to_json
-  puts "Saving view #{view}"
+  puts "Saving view #{view} in #{database_name}"
 
-  @db.save_doc(local_view_doc)
+  db.save_doc(local_view_doc)
 
 end
 
